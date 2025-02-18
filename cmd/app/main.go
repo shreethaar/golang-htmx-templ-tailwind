331e0e51cdc6bc16d348aa2194e775b91f354329
shreethaar <shreethaara380@gmail.com>
2025-02-19 00:55:14 +0800

    GoDotEnv for manage .env variables

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -2,13 +2,20 @@ package main
 import (
     "net/http"
     "github.com/go-chi/chi/v5"
+    "github.com/joho/godotenv"
     "log/slog"
+    "log"
+    "os"
 )
 
 func main() {
+    if err:=godotenv.Load(); err!=nil {
+        log.Fatal(err)
+    }
+
     router:=chi.NewMux()
     router.Get("/foo",handleFoo)
-    listenAddr:=":3000"
+    listenAddr:=os.Getenv("LISTEN_ADDR")
     slog.Info("HTTP server started","listenAddr",listenAddr)
     http.ListenAndServe(listenAddr,router)
 
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module trial-run
 
 go 1.24.0
 
-require github.com/go-chi/chi/v5 v5.2.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+)
